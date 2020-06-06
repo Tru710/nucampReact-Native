cdb865166644020a4de0db7545c089b8fe1577f3
Jesus T <flareon831@gmail.com>
2020-06-05 22:40:52 -0700

    Activity Indicator

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -9,6 +9,17 @@ import { createStackNavigator, createDrawerNavigator,
     DrawerItems } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 import SafeAreaView from 'react-native-safe-area-view';
+import { connect } from 'react-redux';
+import { fetchCampsites, fetchComments, fetchPromotions, 
+    fetchPartners } from '../redux/ActionCreators';
+
+
+const mapDispatchToProps = {
+    fetchCampsites,
+    fetchComments,
+    fetchPromotions,
+    fetchPartners
+};   
 
 const AboutNavigator = createStackNavigator(
     {
@@ -190,6 +201,14 @@ const MainNavigator = createDrawerNavigator(
 );
 
 class Main extends Component {
+
+    componentDidMount() {
+        this.props.fetchCampsites();
+        this.props.fetchComments();
+        this.props.fetchPromotions();
+        this.props.fetchPartners();
+    }
+
     render() {
         return (
             <View style={{
@@ -230,4 +249,4 @@ const styles = StyleSheet.create({
         fontSize: 24
     }
 });
-export default Main;
+export default connect(null, mapDispatchToProps)(Main);
